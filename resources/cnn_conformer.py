9e92835f8b483c5444e2171ee9d00df88bccfa37
tuyendv <tuyen.duongvan268@gmail.com>
2023-05-08 22:19:00 +0700

    resource is best config (v1)

---
+++ resources/cnn_conformer.py
@@ -0,0 +1,142 @@
+from torch.nn.utils import weight_norm
+import torch.nn.functional as F 
+from torch import nn
+import torch
+from .conformer.encoder import ConformerBlock
+import numpy as np
+
+class SpatialDropout1D(nn.Module):
+    def __init__(self, drop_rate):
+        super(SpatialDropout1D, self).__init__()
+        
+        self.dropout = nn.Dropout2d(drop_rate)
+        
+    def forward(self, inputs):
+        inputs = inputs.permute(0, 2, 1)
+        inputs = self.dropout(inputs.unsqueeze(2)).squeeze(2)
+        inputs = inputs.permute(0, 2, 1)
+        
+        return inputs
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        
+        channels = [39, 128]
+        kernels = [3, 3]
+        convs = []
+        
+        for i, (in_channels, out_channels) in enumerate(zip(channels[:-1], channels[1:])):
+            conv = [
+                nn.Conv1d(
+                    in_channels=in_channels, 
+                    out_channels=out_channels, 
+                    kernel_size=kernels[i],
+                    padding="same",
+                    stride=1),
+                nn.BatchNorm1d(num_features=out_channels),
+                nn.ReLU(),
+                nn.MaxPool1d(
+                    kernel_size=2, 
+                    stride=2)
+            ]            
+            convs += conv
+        
+        self.convs = nn.ModuleList(convs)
+        self.dropout = SpatialDropout1D(0.2)
+
+    
+    def forward(self, inputs):
+        for conv in self.convs:
+            inputs = conv(inputs)
+        inputs = self.dropout(inputs)
+        return inputs
+    
+class ConformerEncoder(nn.Module):
+    def __init__(
+            self,
+            encoder_dim: int = 128,
+            num_layers: int = 4,
+            num_attention_heads: int = 4,
+            feed_forward_expansion_factor: int = 4,
+            conv_expansion_factor: int = 2,
+            feed_forward_dropout_p: float = 0.2,
+            attention_dropout_p: float = 0.2,
+            conv_dropout_p: float = 0.2,
+            conv_kernel_size: int = 15,
+            half_step_residual: bool = True,
+    ):
+        super(ConformerEncoder, self).__init__()
+        self.layers = nn.ModuleList(
+            [
+                ConformerBlock(
+                    encoder_dim=encoder_dim,
+                    num_attention_heads=num_attention_heads,
+                    feed_forward_expansion_factor=feed_forward_expansion_factor,
+                    conv_expansion_factor=conv_expansion_factor,
+                    feed_forward_dropout_p=feed_forward_dropout_p,
+                    attention_dropout_p=attention_dropout_p,
+                    conv_dropout_p=conv_dropout_p,
+                    conv_kernel_size=conv_kernel_size,
+                    half_step_residual=half_step_residual) 
+                for _ in range(num_layers)]
+            )
+        
+    def forward(self, inputs, lengths=None):
+        hiddens = []
+        for layer in self.layers:
+            inputs = layer(inputs)
+            
+            tmp = inputs.mean(dim=1).unsqueeze(1)
+            hiddens.append(tmp)
+        hiddens = torch.cat(hiddens, dim=1)
+        
+        return inputs, hiddens
+    
+
+class CNN_Conformer(nn.Module):
+    def __init__(self, config=None, num_layers=4) -> None:
+        super(CNN_Conformer, self).__init__()
+        self.cnn = CNN()
+        self.weighted_layers = nn.Parameter(torch.randn(1, num_layers))
+        self.conformer = ConformerEncoder(
+            encoder_dim=128,
+            num_layers=num_layers,
+            num_attention_heads=4,
+            feed_forward_expansion_factor=4,
+            conv_expansion_factor=2,
+            feed_forward_dropout_p=0.2,
+            attention_dropout_p=0.2,
+            conv_dropout_p=0.2,
+            conv_kernel_size=15,
+            half_step_residual=True
+        )
+        self.cls_head = nn.Linear(128, 8)
+        self.weighted_layers = nn.Parameter(torch.randn(1, num_layers))
+        self.dropout = nn.Dropout(0.3)
+
+    def forward(self, inputs, lengths):
+        inputs = self.cnn(inputs)
+        inputs = inputs.transpose(1, 2)
+        inputs, hiddens = self.conformer(inputs)
+        
+        outputs = torch.matmul(self.weighted_layers, hiddens)
+        outputs = outputs.squeeze(1)
+        outputs = self.dropout(outputs)
+        outputs = self.cls_head(outputs)
+
+        return outputs
+        
+        
+if __name__ == "__main__":
+    model = CNN_Conformer()
+    
+    model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+    params = sum([np.prod(p.size()) for p in model_parameters])
+
+    print(model)
+    print("num params: ",params)
+    inputs = torch.randn(8, 39, 128)
+    
+    output = model(inputs, None)
+    print(output.shape)
---
+++ src/models/fastformer.py
@@ -0,0 +1,229 @@
+import math
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+class AttentionPooling(nn.Module):
+    def __init__(self, config):
+        self.config = config
+        super(AttentionPooling, self).__init__()
+        self.att_fc1 = nn.Linear(config.hidden_size, config.hidden_size)
+        self.att_fc2 = nn.Linear(config.hidden_size, 1)
+        self.apply(self.init_weights)
+
+    def init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+        if isinstance(module, nn.Linear) and module.bias is not None:
+            module.bias.data.zero_()
+
+    def forward(self, x, attn_mask=None):
+        bz = x.shape[0]
+        e = self.att_fc1(x)
+        e = nn.Tanh()(e)
+        alpha = self.att_fc2(e)
+        alpha = torch.exp(alpha)
+        if attn_mask is not None:
+            alpha = alpha * attn_mask.unsqueeze(2)
+        alpha = alpha / (torch.sum(alpha, dim=1, keepdim=True) + 1e-8)
+        x = torch.bmm(x.permute(0, 2, 1), alpha)
+        x = torch.reshape(x, (bz, -1))
+        return x
+    
+class FastMultiHeadSelfAttention(nn.Module):
+    def __init__(self, config):
+        super(FastMultiHeadSelfAttention, self).__init__()
+        self.config = config
+        if config.hidden_size % config.num_attention_heads != 0:
+            raise ValueError(
+                "The hidden size (%d) is not a multiple of the number of attention "
+                "heads (%d)" %
+                (config.hidden_size, config.num_attention_heads))
+        self.attention_head_size = int(config.hidden_size / config.num_attention_heads)
+        self.num_attention_heads = config.num_attention_heads
+        self.all_head_size = self.num_attention_heads * self.attention_head_size
+        self.input_dim = config.hidden_size
+
+        self.query = nn.Linear(self.input_dim, self.all_head_size)
+        self.query_att = nn.Linear(self.all_head_size, self.num_attention_heads)
+        self.key = nn.Linear(self.input_dim, self.all_head_size)
+        self.key_att = nn.Linear(self.all_head_size, self.num_attention_heads)
+        self.transform = nn.Linear(self.all_head_size, self.all_head_size)
+
+        self.softmax = nn.Softmax(dim=-1)
+
+        self.apply(self.init_weights)
+
+    def init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+        if isinstance(module, nn.Linear) and module.bias is not None:
+            module.bias.data.zero_()
+
+    def transpose_for_scores(self, x):
+        new_x_shape = x.size()[:-1] + (self.num_attention_heads,
+                                       self.attention_head_size)
+        x = x.view(*new_x_shape)
+        return x.permute(0, 2, 1, 3)
+
+    def forward(self, hidden_states, attention_mask):
+        # batch_size, seq_len, num_head * head_dim, batch_size, seq_len
+        batch_size, seq_len, _ = hidden_states.shape
+        mixed_query_layer = self.query(hidden_states)
+        mixed_key_layer = self.key(hidden_states)
+        # batch_size, num_head, seq_len
+        query_for_score = self.query_att(mixed_query_layer).transpose(1, 2) / self.attention_head_size ** 0.5
+        # add attention mask
+        query_for_score += attention_mask
+
+        # batch_size, num_head, 1, seq_len
+        query_weight = self.softmax(query_for_score).unsqueeze(2)
+
+        # batch_size, num_head, seq_len, head_dim
+        query_layer = self.transpose_for_scores(mixed_query_layer)
+
+        # batch_size, num_head, head_dim, 1
+        pooled_query = torch.matmul(query_weight, query_layer).transpose(1, 2).view(-1, 1,
+                                                                                    self.num_attention_heads * self.attention_head_size)
+        pooled_query_repeat = pooled_query.repeat(1, seq_len, 1)
+        # batch_size, num_head, seq_len, head_dim
+
+        # batch_size, num_head, seq_len
+        mixed_query_key_layer = mixed_key_layer * pooled_query_repeat
+
+        query_key_score = (self.key_att(mixed_query_key_layer) / self.attention_head_size ** 0.5).transpose(1, 2)
+
+        # add attention mask
+        query_key_score += attention_mask
+
+        # batch_size, num_head, 1, seq_len
+        query_key_weight = self.softmax(query_key_score).unsqueeze(2)
+
+        key_layer = self.transpose_for_scores(mixed_query_key_layer)
+        pooled_key = torch.matmul(query_key_weight, key_layer)
+
+        # query = value
+        weighted_value = (pooled_key * query_layer).transpose(1, 2)
+        weighted_value = weighted_value.reshape(
+            weighted_value.size()[:-2] + (self.num_attention_heads * self.attention_head_size,))
+        weighted_value = self.transform(weighted_value) + mixed_query_layer
+
+        return weighted_value
+    
+class PositionwiseFeedForward(nn.Module):
+    def __init__(self, d_in, d_hid, kernel_size, dropout=0.1):
+        super().__init__()
+
+        # Use Conv1D
+        # position-wise
+        self.w_1 = nn.Conv1d(
+            d_in,
+            d_hid,
+            kernel_size=kernel_size[0],
+            padding=(kernel_size[0] - 1) // 2,
+        )
+        # position-wise
+        self.w_2 = nn.Conv1d(
+            d_hid,
+            d_in,
+            kernel_size=kernel_size[1],
+            padding=(kernel_size[1] - 1) // 2,
+        )
+
+        self.layer_norm = nn.LayerNorm(d_in)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        # residual = x
+        output = x.transpose(1, 2)
+        output = self.w_2(F.relu(self.w_1(output)))
+        output = output.transpose(1, 2)
+        output = self.dropout(output)
+        # output = self.layer_norm(output + residual)
+
+        return output
+    
+class EncoderLayer(nn.Module):
+    def __init__(self, d_model, ffn_hidden, n_head, drop_prob):
+        super(EncoderLayer, self).__init__()
+        self.attention = FastMultiHeadSelfAttention(
+            d_model, n_head, drop_prob)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.dropout1 = nn.Dropout(p=drop_prob)
+
+        self.ffw = PositionwiseFeedForward(d_in=d_model, d_hid=ffn_hidden, kernel_size=(5, 1), dropout=drop_prob)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.dropout2 = nn.Dropout(p=drop_prob)
+
+    def forward(self, x, s_mask):
+        _x = x
+        x = self.attention(Q=x, K=x, V=x, attention_masks=s_mask)
+        
+        # 2. add and norm
+        x = self.dropout1(x)
+        x = self.norm1(x + _x)
+        
+        # 3. positionwise feed forward network
+        _x_ = x
+        x = self.ffw(x)
+      
+        # 4. add and norm
+        x = self.dropout2(x)
+        x = self.norm2(x + _x_)
+        
+        # x = self.sigmoid(x)
+        return x
+    
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout=0.1, max_len=256):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        
+        self.d_model = d_model
+        if d_model % 2 == 1:
+            d_model += 1
+            
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (- math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :, :self.d_model]
+        return self.dropout(x)
+    
+class Transformer_Encoder(nn.Module):
+    def __init__(self, d_model, ffn_hidden, n_head, n_layers, drop_prob):
+        super(Transformer_Encoder, self).__init__()
+        
+        self.position_embedding = PositionalEncoding(
+            d_model=d_model
+        )
+        
+        self.layers = nn.ModuleList([EncoderLayer(d_model=d_model,
+                                                  ffn_hidden=ffn_hidden,
+                                                  n_head=n_head,
+                                                  drop_prob=drop_prob)
+                                     for _ in range(n_layers)])
+        
+
+    def forward(self, features, attention_masks, device="cuda"):
+        inputs = features
+        
+        if attention_masks is None:
+            attention_masks = torch.ones(inputs.shape[0:2]).to(device)
+        
+        inputs = self.position_embedding(inputs)
+        
+        hiddens = []
+        for layer in self.layers:
+            inputs = layer(inputs, attention_masks)
+            
+            tmp = inputs.mean(dim=1).unsqueeze(1)
+            hiddens.append(tmp)
+            
+        hiddens = torch.cat(hiddens, dim=1)
+        return inputs, hiddens
